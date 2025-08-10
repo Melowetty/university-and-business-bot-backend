0aee7645b0e0da1b33fe4d8a4f0680f9e3b62b3c
Denis Malinin <melowetty@mail.ru>
2025-08-11 00:03:31 +0500

    initial commit

---
+++ src/main/kotlin/ru/sigma/hse/business/bot/UniversityAndBusinessBotBackendApplication.kt
@@ -0,0 +1,11 @@
+package ru.sigma.hse.business.bot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UniversityAndBusinessBotBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<UniversityAndBusinessBotBackendApplication>(*args)
+}

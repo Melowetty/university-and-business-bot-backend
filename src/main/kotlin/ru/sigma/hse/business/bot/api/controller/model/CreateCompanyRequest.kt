66b714722cd523cb35b80907b3792f28a7fd24bc
KNBDan <foxtrifrend@gmail.com>
2025-09-01 18:07:51 +0500

    backend_service: add validation to models in controllers

--- src/main/kotlin/ru/sigma/hse/business/bot/api/controller/model/CreateCompanyRequest.kt
+++ src/main/kotlin/ru/sigma/hse/business/bot/api/controller/model/CreateCompanyRequest.kt
@@ -1,12 +1,17 @@
 package ru.sigma.hse.business.bot.api.controller.model
 
 import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.URL
 
 data class CreateCompanyRequest(
+    @field:NotBlank(message = "Wrong company name format")
     @Schema(description = "Название компании", example = "Ростикс")
     val name: String,
+    @field:NotBlank(message = "Wrong company description format")
     @Schema(description = "Описание компании", example = "Вкусная курочка каждый день только у нас")
     val description: String,
+    @field:URL(message = "Wrong company vacancies link format")
     @Schema(description = "Ссылка на вакансию", example = "https://rabora/rostics")
     val vacanciesLink: String
 )

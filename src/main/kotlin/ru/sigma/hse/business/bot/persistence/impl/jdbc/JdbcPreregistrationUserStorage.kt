b19a1e6335f74fdc06613ee1bb0200ade0cf660e
KNBDan <foxtrifrend@gmail.com>
2025-09-03 22:31:38 +0500

    preregistration: add preregistration user domain, repository and storage classes

---
+++ src/main/kotlin/ru/sigma/hse/business/bot/persistence/impl/jdbc/JdbcPreregistrationUserStorage.kt
@@ -0,0 +1,55 @@
+package ru.sigma.hse.business.bot.persistence.impl.jdbc
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.stereotype.Component
+import ru.sigma.hse.business.bot.domain.entity.PreregistrationUserEntity
+import ru.sigma.hse.business.bot.domain.model.Pageable
+import ru.sigma.hse.business.bot.domain.model.PreregistrationUser
+import ru.sigma.hse.business.bot.exception.preregistration.PreregistrationUserByIdNotFoundException
+import ru.sigma.hse.business.bot.persistence.repository.PreregistrationUserRepository
+
+@Component
+class JdbcPreregistrationUserStorage(
+    private val preregistrationUserRepository: PreregistrationUserRepository,
+) {
+    fun getPreregistrationUser(tgId: Long): PreregistrationUser? {
+        if (preregistrationUserRepository.existsById(tgId)) {
+            logger.info { "Found preregistration user with id $tgId" }
+            return preregistrationUserRepository.findById(tgId).get().toPreregistrationUser()
+        }
+
+        logger.warn { "Preregistration user with id $tgId does not exist" }
+        return null
+    }
+
+    fun createPreregistrationUser(tgId: Long): PreregistrationUser {
+        val preregistrationUserEntity = preregistrationUserRepository.save(
+            PreregistrationUserEntity(
+                tgId = tgId
+            )
+        )
+
+        logger.info { "Created preregistration user with id ${preregistrationUserEntity.tgId}" }
+        return preregistrationUserEntity.toPreregistrationUser()
+    }
+
+    fun deletePreregistrationUser(tgId: Long) {
+        if (!preregistrationUserRepository.existsById(tgId)) {
+            logger.warn { "Preregistration user with id $tgId does not exist" }
+            throw PreregistrationUserByIdNotFoundException(tgId)
+        }
+
+        logger.info { "Deleting preregistration user with id $tgId" }
+        preregistrationUserRepository.deleteById(tgId)
+    }
+
+    companion object {
+        private val logger = KotlinLogging.logger {  }
+
+        fun PreregistrationUserEntity.toPreregistrationUser(): PreregistrationUser {
+            return PreregistrationUser(
+                tgId = this.tgId
+            )
+        }
+    }
+}

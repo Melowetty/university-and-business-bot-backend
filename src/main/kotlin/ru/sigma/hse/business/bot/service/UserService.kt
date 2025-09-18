b7c337ed9cb11cf3300354d8196da64381a3b7b3
KNBDan <113082552+KNBDan@users.noreply.github.com>
2025-09-18 13:26:36 +0500

    Update UserService.kt

--- src/main/kotlin/ru/sigma/hse/business/bot/service/UserService.kt
+++ src/main/kotlin/ru/sigma/hse/business/bot/service/UserService.kt
@@ -1,7 +1,6 @@
 package ru.sigma.hse.business.bot.service
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional

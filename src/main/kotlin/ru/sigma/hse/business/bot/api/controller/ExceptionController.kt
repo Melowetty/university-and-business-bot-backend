0960bbf94f39b635668c99f303e71f02e6a9df46
KNBDan <foxtrifrend@gmail.com>
2025-09-22 16:56:38 +0500

    user-points-registration: add exception handler

--- src/main/kotlin/ru/sigma/hse/business/bot/api/controller/ExceptionController.kt
+++ src/main/kotlin/ru/sigma/hse/business/bot/api/controller/ExceptionController.kt
@@ -1,5 +1,6 @@
 package ru.sigma.hse.business.bot.api.controller
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -14,6 +15,7 @@ class ExceptionController {
     @ExceptionHandler(produces = ["application/json"])
     @ResponseStatus(HttpStatus.NOT_FOUND)
     fun handleEntityNotFound(ex: NotFoundException): ErrorResponse {
+        logger.warn(ex) { "Handled entity found exception" }
         return ErrorResponse(
                 ex.errorType,
                 ex.message
@@ -23,6 +25,7 @@ class ExceptionController {
     @ExceptionHandler(produces = ["application/json"])
     @ResponseStatus(HttpStatus.CONFLICT)
     fun handleAlreadyExists(ex: AlreadyExistsException): ErrorResponse {
+        logger.warn(ex) { "Handled already exist exception" }
         return ErrorResponse(
                 ex.errorType,
                 ex.message
@@ -32,10 +35,21 @@ class ExceptionController {
     @ExceptionHandler(produces = ["application/json"])
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handleBadArgument(ex: BadArgumentException): ErrorResponse {
+        logger.warn(ex) { "Handled bad argument exception" }
         return ErrorResponse(
                 ex.errorType,
                 ex.message
             )
+    }
+
+    @ExceptionHandler(produces = ["application/json"])
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleException(ex: Exception): ErrorResponse {
+        logger.warn(ex) { "Handled exception" }
+        return ErrorResponse(
+            "INTERNAL_SERVER_ERROR",
+            "Internal server error"
+        )
     }
 
     @Schema(description = "Ошибка")
@@ -45,4 +59,8 @@ class ExceptionController {
         @Schema(description = "Сообщение об ошибке", example = "User with id 1 not found")
         val message: String,
     )
+
+    companion object {
+        private val logger = KotlinLogging.logger { }
+    }
 }
